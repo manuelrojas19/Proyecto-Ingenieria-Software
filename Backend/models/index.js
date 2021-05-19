8f15c3a56fed5a9f0bec0ff3e0eafb6ad7c2758e
Manuel Rojas Ramos <59751051+manuelrojas19@users.noreply.github.com>
2021-05-19 08:10:47 -0500

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 // Change to production in gcloud
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
