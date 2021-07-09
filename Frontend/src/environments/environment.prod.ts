e1ef864243688e0eb58f069af7412a30abfb3e89
manuelrojas19 <manuelarr99@gmail.com>
2021-07-08 23:39:30 -0500

    Corrección html formulario de comisiones

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sistema-viaticos-backend.uc.r.appspot.com/api/v1',
+  API_URL: 'https://sistema-viaticos-backend.uc.r.appspot.com/api/v2',
 };
